046041fd0a3efb265617258e88e9e9728351cac6
David Komer <david.komer@gmail.com>
2017-07-19 17:01:32 +0300

    closes #8

--- src/app/modules/drum_machine/DrumMachine_Timer.ts
+++ src/app/modules/drum_machine/DrumMachine_Timer.ts
@@ -1,66 +1,58 @@
 
 import { CanvasWidth, CanvasHeight } from "../../main/Main";
+import {PrimitiveFuncs} from "../../../lib/funcs/PrimitiveFuncs";
 import {Row_UI, Block_UI} from "./DrumMachine_UI";
-
 import { MillisecondsTimerSystem, Cell, Transaction, CellLoop, CellSink, Stream, StreamSink} from "sodiumjs";
-
-class Block {
-    public dispose:() => void;
-
-    constructor(displayTarget:Block_UI, cActive:Cell<boolean>) {
-        Transaction.run((): void => {
-            this.dispose = cActive.listen(selected => displayTarget.redraw(selected));
-        });
-    }
-
-}
+import * as R from "ramda";
 
 export class Timer extends PIXI.Container {
+    public dispose:() => void;
+
     private _cMeasure:Cell<number>;
-
     private _row:Row_UI;
-
+    
     constructor() {
         super();
 
         //visual setup
         this._row = new Row_UI(0xff0000, 0xcccccc);
-        this.addChild(this._row);
-
-        
+        this.addChild(this._row);   
     }
 
     public start(cSpeed:Cell<number>) {
+        //just tinkered till it felt okay
+        const getSpeed = (speedFactor:number) => R.clamp(50,1000,(Math.pow((1-speedFactor),3)) * 1000);
+
         //timer setup
         const sys = new MillisecondsTimerSystem();
-        const period = 200;
-
         const time = sys.time;
         const oAlarm = new CellLoop<number>();
         const sAlarm = sys.at(oAlarm);
 
-        //TODO - map period based on cSpeed
-        const nextAlarm = sAlarm.map(t => t + period);
-        oAlarm.loop(nextAlarm.hold(time.sample() + period)); //time.sample() == Date.now()
+        const nextAlarm = sAlarm.snapshot3(oAlarm, cSpeed, (ms,alarmTime, speedFactor) => alarmTime + getSpeed(speedFactor));
+        
+        
+        //set the first alarm. time.sample() is Date.now()
+        oAlarm.loop(nextAlarm.hold(time.sample() + getSpeed(cSpeed.sample())));
 
+        //create a cell to represet the measure, which is updated by the alarm
         const cMeasure = new CellLoop<number>();
         
         cMeasure.loop(
-            nextAlarm.snapshot(cMeasure, (rawTime,measureTime) => {
-                return (measureTime < 15) ? measureTime + 1 : 0;
-            })
+            nextAlarm.snapshot(cMeasure, (rawTime,measureTime) => (measureTime < 15) ? measureTime + 1 : 0)
             .hold(0));
         
         this._cMeasure = cMeasure;
 
-        
-        //get blocks as frp logic
-        const blocks = this._row.children
-            .forEach((graphics, index) => 
-                new Block(graphics as Block_UI, cMeasure.map(num => num === index)));
+        //visual updates when measure changes
+        this.dispose = this._cMeasure.listen(measure => {
+            this._row.children
+                .forEach((block, index) => (block as Block_UI).redraw(measure === index));
+        })
     }
 
     public get cMeasure():Cell<number> {
         return this._cMeasure;
     }
+
 }
--- src/lib/funcs/PrimitiveFuncs.ts
+++ src/lib/funcs/PrimitiveFuncs.ts
@@ -3,14 +3,30 @@ import * as R from "ramda";
 export class PrimitiveFuncs {
     //takes a padding and a list of numbers
     //adds the padding into them sequentially increasing to give it space
-    public static addPadding(pad:number, vals:Array<number>):Array<number> {
-        
+    public static addPadding(pad: number, vals: Array<number>): Array<number> {
+
         let mapIndexed = R.addIndex(R.map);
         return mapIndexed((val, idx) => val + (idx * pad), vals);
     }
 
     //accumulates the total of property prop in all the vals - with a leading zero
-    public static accPropsLeadingZero(prop:string, vals:Array<any>):Array<number> {
+    public static accPropsLeadingZero(prop: string, vals: Array<any>): Array<number> {
         return R.scan(R.add, 0, R.pluck(prop, vals));
+    }
+
+    //adapted from https://stackoverflow.com/questions/846221/logarithmic-slider
+    public static logarithmic(position:number): number {
+        // position will be between 0 and 100
+  var minp = 0;
+  var maxp = 1;
+
+  // The result should be between 100 an 10000000
+  var minv = Math.log(0);
+  var maxv = Math.log(1);
+
+  // calculate adjustment factor
+  var scale = (maxv-minv) / (maxp-minp);
+
+  return Math.exp(minv + scale*(position-minp));
     }
 }
