fb63b4a80d1b5b176d8244dcff3437e02d8d85cf
dakom <david.komer@gmail.com>
2017-08-29 22:05:45 +0300

    cleanup

--- .fusebox/cache/2.2.2/F6gbH-playground/static/app%2Fmodules%2Fsimple%2FSimple_UI.js
+++
@@ -1,7 +0,0 @@
-module.exports = { contents: "\"use strict\";\r\nObject.defineProperty(exports, \"__esModule\", { value: true });\r\nfunction UI_Ball(radius) {\r\n    var graphics = new PIXI.Graphics();\r\n    graphics.beginFill(0xFF0000);\r\n    graphics.drawCircle(0, 0, radius);\r\n    graphics.endFill();\r\n    return graphics;\r\n}\r\nexports.UI_Ball = UI_Ball;\r\n//# sourceMappingURL=Simple_UI.js.map",
-dependencies: [],
-sourceMap: "{\"version\":3,\"file\":\"app/modules/simple/Simple_UI.js\",\"sourceRoot\":\"\",\"sources\":[\"/src/app/modules/simple/Simple_UI.ts\"],\"names\":[],\"mappings\":\";;AAAA,iBAAwB,MAAa;IACjC,IAAM,QAAQ,GAAG,IAAI,IAAI,CAAC,QAAQ,EAAE,CAAC;IACrC,QAAQ,CAAC,SAAS,CAAC,QAAQ,CAAC,CAAC;IAC7B,QAAQ,CAAC,UAAU,CAAC,CAAC,EAAE,CAAC,EAAE,MAAM,CAAC,CAAC;IAClC,QAAQ,CAAC,OAAO,EAAE,CAAC;IACnB,MAAM,CAAC,QAAQ,CAAC;AACpB,CAAC;AAND,0BAMC\"}",
-headerContent: undefined,
-mtime: 1504029030253,
-devLibsRequired : undefined
-};
