dae9856dc8511360191b3b8ffeaa18932e232ff6
dakom <david.komer@gmail.com>
2017-08-31 21:11:27 +0300

    updates

--- src/tests/sodium/map/Mapping.test.ts
+++ src/tests/sodium/map/Mapping.test.ts
@@ -6,43 +6,56 @@ export class MappingTest {
     'Mapping' () {
         new _MappingTest();
     }
+
+    'check order'(done) {
+        checkOrder(done);
+    }
+    
 }
 
 class _MappingTest {
     'check order'(done) {
-        let idx = 0;
-        const expectedOrder = [
-            true,
-            false,
-            false,
-            true,
-            true,
-            false,
-            true
-        ]
-        const sStart = new StreamSink<void>();
-        const sEnd = new StreamSink<void>();
-
-        const cEnabled = sStart
-            .map(n => false)
-            .orElse(
-            sEnd.map(n => true)
-            ).hold(true)
-
-
-        cEnabled.listen(flag => {
-            const expectedFlag = expectedOrder[idx++];
-
-            expect(flag).to.equal(expectedFlag);
-                done();
-        });
-
-
-        sStart.send(undefined);
-        sStart.send(undefined);
-        sEnd.send(undefined);
-        sEnd.send(undefined);
-        sStart.send(undefined);
-        sEnd.send(undefined);
+        checkOrder(done);        
     }
 }
+
+
+function checkOrder(done) {
+    let idx = 0;
+    const expectedOrder = [
+        true,
+        false,
+        false,
+        true,
+        true,
+        false,
+        true
+    ];
+    const out = [];
+    const sStart = new StreamSink<void>();
+    const sEnd = new StreamSink<void>();
+
+    const cEnabled = sStart
+        .map(n => false)
+        .orElse(
+        sEnd.map(n => true)
+        ).hold(true)
+
+
+    cEnabled.listen(flag => {
+        expect(flag).to.equal(expectedOrder[out.length]);
+        out.push(flag);
+        if(out.length === expectedOrder.length) {
+            done();
+        }
+    });
+
+
+    sStart.send(undefined);
+    sStart.send(undefined);
+    sEnd.send(undefined);
+    sEnd.send(undefined);
+    sStart.send(undefined);
+    sEnd.send(undefined);
+}
+
